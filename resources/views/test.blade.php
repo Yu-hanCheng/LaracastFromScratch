2b809515af162cf881cb5de7ae41c9979ac21183
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-04-02 12:09:20 +0800

    [Routing-6] test blade htmlspecialchar

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,6 +6,10 @@
     <title>Test</title>
 </head>
 <body>
-    <h1>Test page</h1>
+    <h1><?= htmlspecialchars($name, ENT_QUOTES) ?></h1>
+    <h1>the name: {{ $name }}</h1>
+    <!-- http://laracastfromscratch.test/test?name=%3Cscript%3Ealert(%27hello%27);%3C/script%3E -->
+    <!-- <h1><?php #echo $name ?></h1> -->
+    <h1>{{!! $name !!}}</h1>  <!-- 有 alert -->
 </body>
 </html>
