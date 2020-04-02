0696ad84f650b334bb8f43791a4990a0d0cf0850
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-04-02 17:57:38 +0800

    [views-15] Refactor with layout

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('posts/{post}','PostController@show');
 Route::get('contact', function(){
     return view('contact');
 });
+Route::get('about', function () {
+    return view('about');
+});
